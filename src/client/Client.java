56e4543814029e48a04651decf0951027570f9be
Alexey Arefev <aref307@gmail.com>
2020-09-10 11:39:31 +0300

    ver1

---
+++ src/client/Client.java
@@ -0,0 +1,50 @@
+package client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket("localhost", 3443);
+            // входящий поток
+            Scanner in =  new Scanner(socket.getInputStream());
+            // исходящий поток
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            // с консоли
+            Scanner sc = new Scanner(System.in);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String msg = in.nextLine();
+                        System.out.println("Сервер: " + msg);
+                    }
+                }
+            }).start();
+
+            while (true) {
+                System.out.println("Напишите свое сообщение");
+                String msg = sc.nextLine();
+                out.println(msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
